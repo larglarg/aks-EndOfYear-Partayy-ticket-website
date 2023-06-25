4cbc0edb44cb32da818a85e71973c3125fa9c847
larglarg <81179677+larglarg@users.noreply.github.com>
2023-06-25 09:17:09 +0200

    Update Mensch.php
    Mensch.php
    add active order
    addid in bestellung
    add wirt id in main db

--- Mensch.php
+++ Mensch.php
@@ -1,6 +1,7 @@
 <?php
 class Mensch
 {
+
     protected $name;
     protected $vorname;
     protected $gb_datum;
@@ -8,24 +9,14 @@ class Mensch
     protected $hash;
     protected $schul_id;
     protected $id;
+    protected $resverierungID;
     public function __construct($params = array()) {
         $this->name = $params['name'];
         $this->vorname = $params['vorname'];
         $this->gb_datum = $params['gb_datum'];
         $this->email = $params['email'];
     }
-    
-    /**
-     * Set the value of hash
-     *
-     * @return  self
-     */
-    public function setHash($hash)
-    {
-        $this->hash = $hash;
 
-        return $this;
-    }
 
     public function setSchul_id($schul_id)
     {
@@ -67,64 +58,170 @@ class Mensch
         }
 
     }
-    public function doseUserExist($conn){
-        $stmt = $conn->prepare("SELECT id, name, vorname, gb_datum, email FROM menschen WHERE name = :name AND vorname = :vorname AND gb_datum = :gb_datum AND email = :email;");
-        $stmt->bindParam(':vorname', $this->vorname);
-        $stmt->bindParam(':name', $this->name);
-        $stmt->bindParam(':gb_datum', $this->gb_datum);
-        $stmt->bindParam(':email', $this->email);
+  /*  public function doseUserExist($conn){
+        $stmt = $conn->prepare("SELECT id, name, vorname, gb_datum, email FROM menschen WHERE name = :name AND vorname = :vorname AND gb_datum = :gb_datum AND email = :email AND schul_id = :schul_id;");
+        $stmt->bindParam(':vorname', $this->vorname, PDO::PARAM_INT);
+        $stmt->bindParam(':name', $this->name, PDO::PARAM_INT);
+        $stmt->bindParam(':gb_datum', $this->gb_datum, PDO::PARAM_INT);
+        $stmt->bindParam(':email', $this->email, PDO::PARAM_INT);
+        $stmt->bindParam(':schul_id', $this->schul_id, PDO::PARAM_INT);
         $stmt->execute();
         if($stmt->rowCount() == 0){
             return false;
         }elseif($stmt->rowCount() == 1){
             return true;
         }else{
+            echo "ok wtf who ??";
             exit();
         }
 
 
 
     }
+    */
+    #typeOfGuast 0-> besteller 1-> gast1 2->gast2 ....
+    public function idInBestellung($conn, $typeOfGuast) {
+        $columnName = '';
+        switch ($typeOfGuast) {
+            case 0:
+                $columnName = 'besteller_id';
+                break;
+            case 1:
+                $columnName = 'gast1_id';
+                break;
+            case 2:
+                $columnName = 'gast2_id';
+                break;
+            case 3:
+                $columnName = 'gast3_id';
+                break;
+            case 4:
+                $columnName = 'gast4_id';
+                break;
+        }
+    
+        $stmt = $conn->prepare("UPDATE bestellung SET $columnName = :id  WHERE id = :reservierung_id;");
+        $stmt->bindParam(':reservierung_id', $this->resverierungID, PDO::PARAM_INT);
+        $stmt->bindParam(':id', $this->id, PDO::PARAM_INT);
+        $stmt->execute();
+    }
+    public function userExestiertKomplett($conn){
+        $stmt = $conn->prepare("SELECT id, email, name, vorname, gb_datum FROM menschen WHERE email = :email AND name = :name AND vorname = :vorname AND gb_datum = :gb_datum;");
+        $stmt->bindParam(':vorname', $this->vorname);
+        $stmt->bindParam(':name', $this->name);
+        $stmt->bindParam(':gb_datum', $this->gb_datum);
+        $stmt->bindParam(':email', $this->email);
+        $stmt->execute();
+        if($stmt->rowCount() != 0){
+
+            return true;
+        }else{
+        return false;
+    }
+    }
+    public function SwitchIDtoexistig($conn){
+        $stmt = $conn->prepare("SELECT id, email, name, vorname, gb_datum FROM menschen WHERE email = :email AND name = :name AND vorname = :vorname AND gb_datum = :gb_datum;");
+        $stmt->bindParam(':vorname', $this->vorname);
+        $stmt->bindParam(':name', $this->name);
+        $stmt->bindParam(':gb_datum', $this->gb_datum);
+        $stmt->bindParam(':email', $this->email);
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $this->id = $row['id'];
+        echo "user is switched to ".$this->id;
+
+    }
+
+    private function checkIDforOrder($conn, $id){
+        $stmt = $conn->prepare("SELECT status, mensch_id FROM main WHERE status != 'frei' AND mensch_id = :id;");
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+        if($stmt->rowCount() != 0){
+            return true;
+        }
+        return false;
+    }
+    
+    
     #$welcher -> 1 mail; 2 name vorname gb datum; 3 mit Mensch ID 
     public function activeOrder($conn, $welcher){
-        if($welcher == 1){
+        switch($welcher){
+            case 1:
+                $stmt = $conn->prepare('SELECT id, email, email_verified FROM menschen WHERE email = :email AND email_verified = 1;');
+                $stmt->bindParam(':email', $this->email, PDO::PARAM_STR);
+                $stmt->execute();
+                $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                foreach ($rows as $row) {
+                    if($this->checkIDforOrder($conn, $row['id'])){
+                        return true;
+                    }
+                }
+            case 2:
+                $stmt = $conn->prepare('SELECT id, name, vorname, gb_datum FROM menschen WHERE name = :name AND vorname = :vorname AND gb_datum = :gb_datum; AND email_verified = 1');
+                $stmt->bindParam(':vorname', $this->vorname, PDO::PARAM_STR);
+                $stmt->bindParam(':name', $this->name, PDO::PARAM_STR);
+                $stmt->bindParam(':gb_datum', $this->gb_datum, PDO::PARAM_STR);
+                $stmt->execute();
+                $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                foreach ($rows as $row) {
+                    if($this->checkIDforOrder($conn, $row['id'])){
+                        return true;
+                    }
+                }
+            case 3:
+                if($this->checkIDforOrder($conn, $this->id)){
+                    return true;
+                }
+
+        }
+        return false;
+
+    /*    if($welcher == 1){
             $stmt = $conn->prepare("SELECT id, email FROM menschen WHERE email = :email AND email_verified = 1 ;");
             $stmt->bindParam(':email', $this->email);
+            $stmt->execute();
         }elseif($welcher == 2){
             $stmt = $conn->prepare("SELECT id, name, vorname, gb_datum FROM menschen WHERE name = :name AND vorname = :vorname AND gb_datum = :gb_datum;");
             $stmt->bindParam(':vorname', $this->vorname);
             $stmt->bindParam(':name', $this->name);
             $stmt->bindParam(':gb_datum', $this->gb_datum);
+            $stmt->execute();
         }elseif($welcher == 3){
-            $stmt = $conn->prepare("SELECT besteller_id, gast1_id, gast2_id, gast3_id, gast4_id, status FROM bestellung WHERE (status != 'frei' OR status != '') AND (besteller_id = :id OR gast1_id = :id OR gast2_id = :id OR gast3_id = :id OR gast4_id = :id);");
-            $stmt->bindParam(':id',$this->id);
+
 
         }
         
-        $stmt->execute();
-        $stmtCheckForActiv = $conn->prepare("SELECT id, mensch_id, status FROM Main WHERE mensch_id = :id AND stats != 'frei;'");
+        
+        $stmtCheckForActiv = $conn->prepare("SELECT id, status, mensch_id, status FROM Main WHERE mensch_id = :id AND status != 'frei;'");
         if($welcher == 3){
-            $stmtCheckForActiv = $conn->prepare("SELECT id, mensch_id, reservierung_id status FROM Main WHERE mensch_id = :mensch_id AND reservierung_id = :reservierung_id AND stats != 'frei;'");
-        }
-        foreach($stmt->fetch(PDO::FETCH_ASSOC) as $row){
-            if($welcher == 3){
-                $stmtCheckForActiv->bindParam(':reservierung_id', $row['id']);
-                $stmtCheckForActiv->bindParam('mensch_id', $this->id);
+            $stmtCheckForActiv->bindParam(':id', $this->id);
+            $stmtCheckForActiv->execute();
+            if($stmtCheckForActiv->rowCount() != 0){
+                return true;
             }else{
+                return false;
+            }
+        }else{
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($rows as $row) {
+
            
-            $stmtCheckForActiv->bindParam(':mensch_id', $row['id']);
-        }
+            $stmtCheckForActiv->bindParam(':id', $row['id']);
             $stmtCheckForActiv->execute();
             if($stmtCheckForActiv->rowCount() != 0){
                 return true;
             }
+
         }
         return false;
+        }
+        */
+        
     }
     public function generateHash($hashseed){
         $this->hash = hash('sha3-512', $this->name . $this->vorname .  $this->gb_datum . $this->email . $hashseed, false);
     }
-    public function writeInDB($conn) {
+    public function writeMenschInDB($conn) {
         $stmt = $conn->prepare("INSERT INTO menschen (name, vorname, gb_datum, schule_id, email, hash) VALUES (:name, :vorname, :gb_datum, :schul_id, :email, :hash)");
         $stmt->bindParam(':name', $this->name);
         $stmt->bindParam(':vorname', $this->vorname);
@@ -133,9 +230,16 @@ class Mensch
         $stmt->bindParam(':email', $this->email);
         $stmt->bindParam(':hash', $this->hash);
         $stmt->execute();
-        $row = $stmt->fetch(PDO::FETCH_ASSOC);
-        $this->id = $row["id"];
+        
+        $this->id = $conn->lastInsertId();
     }
+    public function writeIDInMainDB($conn) {
+        $stmt = $conn->prepare("UPDATE main SET mensch_id = :id WHERE reservierung_id = :reservierung_id LIMIT 1;");
+        $stmt->bindParam(':id', $this->id , PDO::PARAM_INT);
+        $stmt->bindParam(':reservierung_id', $this->resverierungID, PDO::PARAM_INT);
+        $stmt->execute();
+    }
+
     /*$stmtCheck = $conn->prepare("SELECT id, besteller_id, gast1_id, gast2_id, gast3_id, gast4_id, status FROM bestellung WHERE besteller_id = ':localID' OR gast1_id = ':localID' OR gast2_id = ':localID' OR gast3_id = ':localID' OR gast4_id = ':localID';");
             $stmtMain = $conn->prepare("SELECT Status, mensch_id, reservierung_id WHERE Status != 'frei' AND reservierung_id = :reservierungs_id AND mensch_id = :localID;");
             foreach($stmt->fetch(PDO::FETCH_ASSOC) as $row){
@@ -170,5 +274,33 @@ class Mensch
             */
 
 
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of resverierungID
+     *
+     * @return  self
+     */ 
+    public function setResverierungID($resverierungID)
+    {
+        $this->resverierungID = $resverierungID;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of hash
+     */ 
+    public function getHash()
+    {
+        return $this->hash;
+    }
 }
 ?>
