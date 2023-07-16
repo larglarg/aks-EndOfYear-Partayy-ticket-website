96a1aed42817cbb61538bd61a77f517616ffd2d7
larglarg <lars.handwerker@web.de>
2023-07-16 11:07:28 +0200

    style try

--- Send_QE_code.php
+++ Send_QE_code.php
@@ -2,7 +2,10 @@
 
 $QRpath = $_GET['path'];
 $bestellungsHash = $_GET['bestellungsHash'];
-$QRpath = "http://localhost/aks-EndOfYear-Partayy-ticket-website/aks-EndOfYear-Partayy-ticket-website/".substr($QRpath, 1);
+
+// Additional text to be included in the email
+$additionalText = "Bitte beachten Sie, dass Sie mit dem QR-Code innerhalb der nächsten 5 Tage die Karten im Anne Frank Haus in Karlsruhe abholen und bezahlen können. Vergessen Sie nicht, den unterschriebenen Mutterzettel mitzubringen.";
+
 ?>
 
 <!DOCTYPE html>
@@ -10,105 +13,36 @@ $QRpath = "http://localhost/aks-EndOfYear-Partayy-ticket-website/aks-EndOfYear-P
 
 <head>
     <meta charset="UTF-8">
-    <title>Ticketreservierung</title>
+    <title></title>
     <style>
-        .container {
-            max-width: 600px;
-            margin: 0 auto;
-            padding: 20px;
-        }
-
-        h1 {
-            color: #63007F;
-        }
-
-        label {
-            display: block;
-            margin-top: 10px;
-        }
-
-        input[type="number"] {
-            width: 100%;
-            padding: 5px;
-            margin-top: 5px;
-            border: 1px solid #E0E0E0;
-        }
-
-        .agb,
-        .EinzeldAbholen,
-        .einwilligung {
-            margin-top: 10px;
-        }
-
-        input[type="checkbox"] {
-            margin-right: 5px;
-        }
-
-        input[type="submit"] {
-            margin-top: 20px;
-            background-color: #63007F;
-            color: #FFFFFF;
-            border: none;
-            padding: 10px 20px;
-            cursor: pointer;
-        }
-
-        a {
-            color: #63007F;
-        }
-
-        .instagram-icon {
-            display: inline-block;
-            width: 16px;
-            height: 16px;
-            background-image: url('./ig-logo-bw.png');
-            /* Pfad zum Instagram-Logo */
-            background-size: cover;
-            margin-right: 5px;
-            vertical-align: middle;
-        }
-
-        .button-submit {
-            justify-content: center;
-            display: flex;
-            align-items: center;
-            margin-top: 20px;
-            background-color: #63007F;
-            color: #FFFFFF;
-            border: none;
-            padding: 10px 20px;
-            cursor: pointer;
-        }
-
-        .link-button {
-            background: none;
-            border: none;
-            color: blue;
-            text-decoration: underline;
-            cursor: pointer;
-            font-size: 1em;
-            font-family: serif;
-            margin-left: 5px;
-            margin-left: 5px;
-            padding: 0px 0px;
-        }
-
-        .link-button:focus {
-            outline: none;
-        }
-
-        .link-button:active {
-            color: red;
-        }
+    <?php
+    include 'style.css';
+    ?>
     </style>
-    <link rel="icon" href="https://bestellungstest.larglarg.com/aks-EndOfYear-Partayy-ticket-website-test/images.png">
-    <!-- Ersetze "https://example.com" durch die tatsächliche URL deines Servers -->
+
 </head>
 
 <body>
-    <div style="max-width: 600px; margin: 0 auto; padding: 20px;">
-        <h1 style="color: #63007F;">QR code Mail Wurde gesendet</h1>
-        <img src="<?PHP echo $QRpath; ?>" alt="QR-Code" style="width:auto;">
+
+
+    <div class="container">
+        <div class="content">
+            <div class="inner-content">
+                <div class="header skew">
+                    <h1>AKS EndOfYear-Partayy</h1>
+                    <h3>Reservierung</h3>
+                </div>
+
+                <img src="<?PHP echo $QRpath; ?>" alt="QR-Code" style="width:auto;">
+
+                <!-- Display the additional text below the QR code -->
+                <p><?php echo $additionalText; ?></p>
+
+            </div>
+        </div>
+        <?php
+        include 'footer.php';
+        ?>
     </div>
 </body>
 
